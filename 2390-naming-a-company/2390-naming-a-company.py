62698cab5a9639e784954e1c0fb4dac10ef7d7e4
omr50 <112574417+omr50@users.noreply.github.com>
2024-10-12 02:44:27 -0400

    Time: 616 ms (36.88%), Space: 31 MB (67.81%) - LeetHub

--- 2390-naming-a-company/2390-naming-a-company.py
+++ 2390-naming-a-company/2390-naming-a-company.py
@@ -8,7 +8,11 @@ class Solution:
         
         res = 0
         # 26 x 26 loop
-        # inner might b n in total
+        # inner might be n in total across all iterations
+        # because for each character in the 26, there could be
+        # from 0 to n suffixes in there. So therefore all iterations
+        # should sum up to n. Since we are doing char 1, and char 2
+        # its then 26x26xn so its really O(n) time.
         for ch1 in char_suffix:
             for ch2 in char_suffix:
                 if ch1 == ch2:
